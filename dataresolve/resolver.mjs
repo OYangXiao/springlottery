6e1d0211866b4f18fb691cda7c48b0b092e632ea
016649 <ouyangxiao@htsc.com>
2021-02-08 17:27:18 +0800

    优化汇总内容

--- dataresolve/resolver.mjs
+++ dataresolve/resolver.mjs
@@ -8,8 +8,9 @@ const timeFormatter = new Intl.DateTimeFormat('zh-CN', {
   hour12: false,
 });
 //调用方法
-var path = 'database_export-yZKySJQ_4IsJ.json';
+var path = 'database_export-o8sFCMxgB2RQ.json';
 read_file(path, function (data) {
+  let lastScan = 0;
   const stats = {
     参与人数: data.length,
     a福: 0,
@@ -22,14 +23,17 @@ read_file(path, function (data) {
     '2个福': 0,
     '3个福': 0,
     '4个福': 0,
-    完成人员列表: [],
   };
+  const spread = {};
   const finished = [];
   data.forEach((user) => {
     const set = new Set();
     let time = undefined;
     user.history.forEach((day) => {
       day.record.forEach((draw) => {
+        if(draw.time > lastScan){
+          lastScan = draw.time
+        }
         stats[`${draw.name}福`]++;
         if (draw.name !== 'e') {
           set.add(draw.name);
@@ -39,17 +43,30 @@ read_file(path, function (data) {
     });
     const hasBlessings = Array.from(set);
     stats[`${hasBlessings.length}个福`]++;
-    if (hasBlessings.length === 4) {
+    if (hasBlessings.length === 4 && user.userinfo.id !== '016649') {
+      const idStart = user.userinfo.id.substring(0, 3);
+      if (spread[idStart] === undefined) {
+        spread[idStart] = 1;
+      } else {
+        spread[idStart]++;
+      }
       finished.push({ ...user.userinfo, time, count: user.history.map((day) => day.record).flat().length });
       finished.sort((a, b) => a.time - b.time);
     }
-    stats.完成人员列表 = finished.map(
-      (user) =>
-        `${user.name}${new Array((4 - user.name.length)*2).fill(' ').join('')}- (${user.id}) - ${
-          user.count
-        }次 - ${timeFormatter.format(new Date(user.time))}`
-    );
   });
+  stats.完成人员列表 = finished.map(
+    (user) =>
+      `${user.name}${new Array(Math.max(4 - user.name.length, 0) * 2).fill(' ').join('')}- (${user.id}) - ${
+        user.count
+      }次 - ${timeFormatter.format(new Date(user.time))}`
+  );
+  stats.完成人员分布 = Object.fromEntries(
+    Object.entries(spread)
+      .sort((a, b) => a[0] - b[0])
+      .map(([key, value]) => [key + 'xxx', value])
+  );
+  stats.最后一次扫码 = timeFormatter.format(new Date(lastScan))
+  stats['4个福']--
   console.log(stats);
 });
 
